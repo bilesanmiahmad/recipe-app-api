7e35fd8937c685645f71e240eac24e69f5461508
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2020-10-01 09:41:11 +0000

    added core app

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,40 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AdminSiteTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@recipeapp.com',
+            password='adminpass123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@recipeapp.com',
+            password='userpass123',
+            name='user name'
+        )
+    
+    def test_user_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_customuser_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_customuser_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEquals(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that create user page works"""
+        url = reverse('admin:core_customuser_add')
+        res = self.client.get(url)
+
+        self.assertEquals(res.status_code, 200)
